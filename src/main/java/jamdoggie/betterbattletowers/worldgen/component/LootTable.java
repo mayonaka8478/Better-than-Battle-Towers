6da6a061dccdd2e5c5a887fbeffcebcc56ca2029
Redart15 <redart15@users.noreply.github.com>
2025-11-13 22:53:02 +0100

    refactored loot table to work with the new config

--- src/main/java/jamdoggie/betterbattletowers/worldgen/component/LootTable.java
+++ src/main/java/jamdoggie/betterbattletowers/worldgen/component/LootTable.java
@@ -4,146 +4,66 @@ import net.minecraft.core.WeightedRandomBag;
 import net.minecraft.core.WeightedRandomLootObject;
 import net.minecraft.core.block.BlockLogicChest;
 import net.minecraft.core.block.Blocks;
-import net.minecraft.core.item.ItemStack;
-import net.minecraft.core.item.Items;
+import net.minecraft.core.item.*;
 import net.minecraft.core.player.inventory.container.Container;
 import net.minecraft.core.util.helper.DyeColor;
 import net.minecraft.core.world.World;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class LootTable {
 	public static final int LAPIZ = DyeColor.BLUE.itemMeta;
 	public static final double INC = 1.29f;
 	public static final int LOOT_AMOUNT = 7;
-	public static int MAX_TIER;
-	public static WeightedRandomBag<WeightedRandomLootObject>[] TOWER_LOOT_TABLE = (WeightedRandomBag<WeightedRandomLootObject>[]) new WeightedRandomBag[10];
+	public static final int MAX_TIER = 10;
+	protected static final WeightedRandomBag<WeightedRandomLootObject>[] TOWER_LOOT_TABLE = (WeightedRandomBag<WeightedRandomLootObject>[]) new WeightedRandomBag[10];
 
-	static {
-		int index = 0;
-		double tier = 1;
-		//floor1
-		TOWER_LOOT_TABLE[index] = new WeightedRandomBag<>();
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.STICK.getDefaultStack(), 3, 8), 30.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.SEEDS_WHEAT.getDefaultStack(), 3, 5), 30.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.AMMO_PEBBLE.getDefaultStack(), 3, 5), 20.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Blocks.SAND.getDefaultStack(), 1, 3), 20.0f * tier);
+	private LootTable(){}
 
-		//floor2
-		index++;
-		tier *= INC;
-		TOWER_LOOT_TABLE[index] = new WeightedRandomBag<>();
-		for (WeightedRandomBag<WeightedRandomLootObject>.Entry entry : TOWER_LOOT_TABLE[index - 1].getEntriesWithWeights()) {
-			TOWER_LOOT_TABLE[index].addEntry(entry.getObject(), entry.getWeight());
+	public static class LootEntry {
+		protected IItemConvertible value;
+		protected int metadata;
+
+		public IItemConvertible getValue() {return value;}
+		public int getMetadata() {return metadata;}
+
+		LootEntry(IItemConvertible value, int metadata) {
+			this.value = value;
+			this.metadata = metadata;
 		}
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.STICK.getDefaultStack(), 3, 10), 30.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.AMMO_PEBBLE.getDefaultStack(), 3, 8), 30.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Blocks.PLANKS_OAK_PAINTED.getDefaultStack(), 1, 3).setRandomMetadata(0, 15), 10.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Blocks.PLANKS_OAK.getDefaultStack(), 1, 3), 10.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Blocks.WOOL.getDefaultStack(), 1, 3).setRandomMetadata(0, 15), 20.0f * tier);
 
-
-		//floor3
-		index++;
-		tier *= INC;
-		TOWER_LOOT_TABLE[index] = new WeightedRandomBag<>();
-		for (WeightedRandomBag<WeightedRandomLootObject>.Entry entry : TOWER_LOOT_TABLE[index - 1].getEntriesWithWeights()) {
-			TOWER_LOOT_TABLE[index].addEntry(entry.getObject(), entry.getWeight());
+		public static LootEntry loot(IItemConvertible value, int metadata) {
+			return new LootEntry(value, metadata);
 		}
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.FEATHER_CHICKEN.getDefaultStack(), 3, 8), 30.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.FOOD_BREAD.getDefaultStack()), 30.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Blocks.MUSHROOM_BROWN.getDefaultStack(), 1, 3), 20.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Blocks.MUSHROOM_RED.getDefaultStack(), 1, 3), 20.0f * tier);
+	}
 
-		//floor4
-		index++;
-		tier *= INC;
-		TOWER_LOOT_TABLE[index] = new WeightedRandomBag<>();
-		for (WeightedRandomBag<WeightedRandomLootObject>.Entry entry : TOWER_LOOT_TABLE[index - 1].getEntriesWithWeights()) {
-			TOWER_LOOT_TABLE[index].addEntry(entry.getObject(), entry.getWeight());
+	public static void createTables(Map<Integer, List<LootEntry>> lootEntries) {
+		double level = 1;
+		for (int jndex = 0; jndex < 10; jndex++, level *= INC) {
+			TOWER_LOOT_TABLE[jndex] = new WeightedRandomBag<>();
+			if (jndex > 0) {
+				for (WeightedRandomBag<WeightedRandomLootObject>.Entry entry : TOWER_LOOT_TABLE[jndex - 1].getEntriesWithWeights()) {
+					TOWER_LOOT_TABLE[jndex].addEntry(entry.getObject(), entry.getWeight());
+				}
+			}
+			List<LootEntry> lootEntryList = lootEntries.getOrDefault(jndex, new ArrayList<>());
+			if (lootEntryList.isEmpty()) continue;
+			int high = lootEntryList.size() / 2;
+			for (int i = 0; i < high; i++) {
+				LootEntry entry = lootEntryList.get(i);
+				int minStacksize = Math.min(3, entry.value.getDefaultStack().getMaxStackSize());
+				int maxStacksize = Math.min(5, entry.value.getDefaultStack().getMaxStackSize());
+				TOWER_LOOT_TABLE[jndex].addEntry(new WeightedRandomLootObject(entry.value.getDefaultStack(), minStacksize, maxStacksize).setRandomMetadata(entry.metadata, entry.metadata), level * 30);
+			}
+			for (int i = high; i < lootEntries.size(); i++) {
+				LootEntry entry = lootEntryList.get(i);
+				int minStacksize = Math.min(3, entry.value.getDefaultStack().getMaxStackSize());
+				int maxStacksize = Math.min(5, entry.value.getDefaultStack().getMaxStackSize());
+				TOWER_LOOT_TABLE[jndex].addEntry(new WeightedRandomLootObject(entry.value.getDefaultStack(), minStacksize, maxStacksize).setRandomMetadata(entry.metadata, entry.metadata), level * 20);
+			}
 		}
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.FEATHER_CHICKEN.getDefaultStack(), 3, 10), 30.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.FOOD_BREAD.getDefaultStack(), 1, 3), 30.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Blocks.MUSHROOM_BROWN.getDefaultStack(), 3, 5), 20.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Blocks.MUSHROOM_RED.getDefaultStack(), 3, 5), 20.0f * tier);
-
-		//floor5
-		index++;
-		tier *= INC;
-		TOWER_LOOT_TABLE[index] = new WeightedRandomBag<>();
-		for (WeightedRandomBag<WeightedRandomLootObject>.Entry entry : TOWER_LOOT_TABLE[index - 1].getEntriesWithWeights()) {
-			TOWER_LOOT_TABLE[index].addEntry(entry.getObject(), entry.getWeight());
-		}
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.BOOK.getDefaultStack(), 1, 5), 30.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.ROPE.getDefaultStack(), 1, 5), 30.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.BRICK_CLAY.getDefaultStack(), 1, 3), 20.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.ORE_RAW_IRON.getDefaultStack(), 1, 3), 20.0f * tier);
-
-		//floor6
-		index++;
-		tier *= INC;
-		TOWER_LOOT_TABLE[index] = new WeightedRandomBag<>();
-		for (WeightedRandomBag<WeightedRandomLootObject>.Entry entry : TOWER_LOOT_TABLE[index - 1].getEntriesWithWeights()) {
-			TOWER_LOOT_TABLE[index].addEntry(entry.getObject(), entry.getWeight());
-		}
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.BOOK.getDefaultStack(), 3, 5), 30.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.FLINT.getDefaultStack(), 1, 5), 30.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.DUST_REDSTONE.getDefaultStack(), 1, 3), 20.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.ORE_RAW_GOLD.getDefaultStack(), 1, 3), 20.0f * tier);
-
-		//floor7
-		index++;
-		tier *= INC;
-		TOWER_LOOT_TABLE[index] = new WeightedRandomBag<>();
-		for (WeightedRandomBag<WeightedRandomLootObject>.Entry entry : TOWER_LOOT_TABLE[index - 1].getEntriesWithWeights()) {
-			TOWER_LOOT_TABLE[index].addEntry(entry.getObject(), entry.getWeight());
-		}
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.BOOK.getDefaultStack(), 3, 8), 30.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.INGOT_IRON.getDefaultStack(), 1, 5), 30.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.INGOT_GOLD.getDefaultStack(), 1, 3), 20.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.BUCKET_LAVA.getDefaultStack()), 20.0f * tier);
-
-		//floor8
-		index++;
-		tier *= INC;
-		TOWER_LOOT_TABLE[index] = new WeightedRandomBag<>();
-		for (WeightedRandomBag<WeightedRandomLootObject>.Entry entry : TOWER_LOOT_TABLE[index - 1].getEntriesWithWeights()) {
-			TOWER_LOOT_TABLE[index].addEntry(entry.getObject(), entry.getWeight());
-		}
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.INGOT_GOLD.getDefaultStack(), 1, 5), 30.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.QUARTZ.getDefaultStack(), 1, 5), 30.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.OLIVINE.getDefaultStack(), 1, 3), 20.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Blocks.TNT.getDefaultStack(), 1, 3), 20.0f * tier);
-
-		//floor9
-		index++;
-		tier *= INC;
-		TOWER_LOOT_TABLE[index] = new WeightedRandomBag<>();
-		for (WeightedRandomBag<WeightedRandomLootObject>.Entry entry : TOWER_LOOT_TABLE[index - 1].getEntriesWithWeights()) {
-			TOWER_LOOT_TABLE[index].addEntry(entry.getObject(), entry.getWeight());
-		}
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.QUARTZ.getDefaultStack(), 1, 5), 30.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.CHAINLINK.getDefaultStack(), 1, 5), 30.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.DYE.getDefaultStack(), 1, 3).setRandomMetadata(LAPIZ, LAPIZ), 20.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.DIAMOND.getDefaultStack(), 1, 3), 20.0f * tier);
-
-		//floor10
-		index++;
-		tier *= INC;
-		TOWER_LOOT_TABLE[index] = new WeightedRandomBag<>();
-		for (WeightedRandomBag<WeightedRandomLootObject>.Entry entry : TOWER_LOOT_TABLE[index - 1].getEntriesWithWeights()) {
-			TOWER_LOOT_TABLE[index].addEntry(entry.getObject(), entry.getWeight());
-		}
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.DUST_REDSTONE.getDefaultStack(), 3, 5), 30.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.DYE.getDefaultStack(), 3, 5).setRandomMetadata(LAPIZ, LAPIZ), 30.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Items.DIAMOND.getDefaultStack(), 3, 5), 20.0f * tier);
-		TOWER_LOOT_TABLE[index].addEntry(new WeightedRandomLootObject(Blocks.MESH_GOLD.getDefaultStack(), 1, 3), 20.0f * tier);
-
-		MAX_TIER = index;
 	}
 
 	public static void populateChest(World world, Random random, int x, int y, int z, int tier
@@ -192,5 +112,81 @@ public class LootTable {
 			}
 		}
 		inventory.setItem(index, itemstack);
+	}
+
+	public static Map<Integer, List<LootTable.LootEntry>> getDefaultMap() {
+		Map<Integer, List<LootTable.LootEntry>> table = new HashMap<>();
+		int tier = 0;
+		table.put(tier, new ArrayList<>());
+		table.get(tier).add(LootTable.LootEntry.loot(Items.STICK, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Items.SEEDS_WHEAT, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Items.AMMO_PEBBLE, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Blocks.SAND, 0));
+		tier++;
+
+		table.put(tier, new ArrayList<>());
+		table.get(tier).add(LootTable.LootEntry.loot(Items.STICK, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Items.AMMO_PEBBLE, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Blocks.PLANKS_OAK_PAINTED, -1));
+		table.get(tier).add(LootTable.LootEntry.loot(Blocks.PLANKS_OAK, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Blocks.WOOL, -1));
+		tier++;
+
+		table.put(tier, new ArrayList<>());
+		table.get(tier).add(LootTable.LootEntry.loot(Items.FEATHER_CHICKEN, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Items.FOOD_BREAD, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Blocks.MUSHROOM_BROWN, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Blocks.MUSHROOM_RED, 0));
+		tier++;
+
+		table.put(tier, new ArrayList<>());
+		table.get(tier).add(LootTable.LootEntry.loot(Items.FEATHER_CHICKEN, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Items.FOOD_BREAD, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Blocks.MUSHROOM_BROWN, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Blocks.MUSHROOM_RED, 0));
+		tier++;
+
+		table.put(tier, new ArrayList<>());
+		table.get(tier).add(LootTable.LootEntry.loot(Items.BOOK, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Items.ROPE, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Items.BRICK_CLAY, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Items.ORE_RAW_IRON, 0));
+		tier++;
+
+		table.put(tier, new ArrayList<>());
+		table.get(tier).add(LootTable.LootEntry.loot(Items.BOOK, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Items.FLINT, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Items.DUST_REDSTONE, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Items.ORE_RAW_GOLD, 0));
+		tier++;
+
+		table.put(tier, new ArrayList<>());
+		table.get(tier).add(LootTable.LootEntry.loot(Items.BOOK, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Items.INGOT_IRON, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Items.INGOT_GOLD, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Items.BUCKET_LAVA, 0));
+		tier++;
+
+		table.put(tier, new ArrayList<>());
+		table.get(tier).add(LootTable.LootEntry.loot(Items.INGOT_GOLD, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Items.QUARTZ, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Items.OLIVINE, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Blocks.TNT, 0));
+		tier++;
+
+		table.put(tier, new ArrayList<>());
+		table.get(tier).add(LootTable.LootEntry.loot(Items.QUARTZ, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Items.CHAINLINK, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Items.DYE, LAPIZ));
+		table.get(tier).add(LootTable.LootEntry.loot(Items.DIAMOND, 0));
+		tier++;
+
+		table.put(tier, new ArrayList<>());
+		table.get(tier).add(LootTable.LootEntry.loot(Items.DUST_REDSTONE, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Items.DYE, LAPIZ));
+		table.get(tier).add(LootTable.LootEntry.loot(Items.DIAMOND, 0));
+		table.get(tier).add(LootTable.LootEntry.loot(Blocks.MESH_GOLD, 0));
+
+		return table;
 	}
 }
