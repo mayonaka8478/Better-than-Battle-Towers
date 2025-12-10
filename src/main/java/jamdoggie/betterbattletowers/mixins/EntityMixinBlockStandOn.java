6c7fff19c24188b5f005ff7bac203ceb39d2dc20
Redart15 <redart15@users.noreply.github.com>
2025-12-11 00:10:34 +0100

    mixin setup for the standon function

---
+++ src/main/java/jamdoggie/betterbattletowers/mixins/EntityMixinBlockStandOn.java
@@ -0,0 +1,8 @@
+package jamdoggie.betterbattletowers.mixins;
+
+import net.minecraft.core.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(value = Entity.class, remap = false)
+public class EntityMixinBlockStandOn{
+}
