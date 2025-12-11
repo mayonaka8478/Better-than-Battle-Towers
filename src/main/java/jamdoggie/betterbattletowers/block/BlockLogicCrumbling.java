6e4a19ec25311eab2cabcbe803745c2e3a82cab0
Redart15 <redart15@users.noreply.github.com>
2025-12-11 19:08:54 +0100

    added crumbling block to be used for flooring in the towers

--- src/main/java/jamdoggie/betterbattletowers/block/BlockLogicCrumbling.java
+++ src/main/java/jamdoggie/betterbattletowers/block/BlockLogicCrumbling.java
@@ -17,23 +17,27 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.Random;
 
-public class BlockLogicUnstable extends BlockLogic {
-	public static float BASE_VALUE_BLAST = 15.0f;
-	public static float BASE_VALUE_HARDNESS = 2000.0f;
+import static jamdoggie.betterbattletowers.worldgen.WorldFeatureTower.BLOCK_AIR;
 
-	public BlockLogicUnstable(Block<?> block, Material material) {
+public class BlockLogicCrumbling extends BlockLogic implements BattleTowerTriggerStandOn {
+	public static final int UPPER_BIT_MASK = 0b1000_0000;
+	public static final int LOWER_BIT_MASK = 0b0000_0011;
+	private static final float BASE_VALUE_BLAST = 15.0f;
+	private static final float BASE_VALUE_HARDNESS = 2000.0f;
+	private final int coolDown;
+
+	public BlockLogicCrumbling(Block<?> block, Material material, float coolDown) {
 		super(block, material);
+		this.coolDown = (int)Math.floor(Math.max(coolDown * Global.TICKS_PER_SECOND, 1));
 	}
 
 	@Override
 	public void updateTick(World world, int x, int y, int z, Random rand) {
 		int metadata = world.getBlockMetadata(x, y, z);
-		if ((metadata >> 7) == 1) {
-			world.setBlockMetadataWithNotify(x, y, z, (metadata & 0b0000_0011));
+		if ((metadata & UPPER_BIT_MASK) == UPPER_BIT_MASK) {
+			world.setBlockMetadataWithNotify(x, y, z, (metadata & LOWER_BIT_MASK));
 		}
 	}
-
-
 
 	@Override
 	public ItemStack @Nullable [] getBreakResult(World world, EnumDropCause dropCause, int meta, TileEntity tileEntity) {
@@ -41,33 +45,43 @@ public class BlockLogicUnstable extends BlockLogic {
 	}
 
 	@Override
+	public void onEntityStandOn(World world, int x, int y, int z, Entity entity) {
+		this.updateBlock(world, x, y, z, entity);
+	}
+
+	@Override
 	public void onEntityWalking(World world, int x, int y, int z, Entity entity) {
+		this.updateBlock(world, x, y, z, entity);
+	}
+
+	private void updateBlock(World world, int x, int y, int z, Entity entity) {
 		if (!(entity instanceof Player)) {
 			return;
 		}
 		int metadata = world.getBlockMetadata(x, y, z);
-		if ((metadata >> 7) == 1) {
+		if ((metadata & UPPER_BIT_MASK) == UPPER_BIT_MASK) {
 			return;
 		}
-		if ((metadata & 0b0000_0011) == 0) {
+		int lowerBits = metadata & LOWER_BIT_MASK;
+		if (lowerBits == 0) {
 			this.spawnParticle(world, x, y, z);
-			world.setBlockWithNotify(x, y, z, 0);
-			world.playBlockSoundEffect(null, x, y + 0.5F, z, this.block, EnumBlockSoundEffectType.MINE);
+			world.setBlockWithNotify(x, y, z, BLOCK_AIR);
+			world.playBlockSoundEffect(null, x, y + 0.5F, z, Blocks.PERMAFROST, EnumBlockSoundEffectType.MINE);
 			return;
 		}
-		world.setBlockMetadataWithNotify(x, y, z, (0b1000_0000) + ((metadata & 0b0000_0011) - 1));
-		this.block.blastResistance = BASE_VALUE_BLAST / (float)Math.pow(4, (3 - (metadata & 0b0000_0011)));
-		this.block.blockHardness = BASE_VALUE_HARDNESS / (float)Math.pow(4, (3 - (metadata & 0b0000_0011)));
-		world.playBlockSoundEffect(null, x, y + 0.5F, z, this.block, EnumBlockSoundEffectType.DIG);
-		world.scheduleBlockUpdate(x, y, z, this.id(), 2 * Global.TICKS_PER_SECOND);
+		world.setBlockMetadataWithNotify(x, y, z, UPPER_BIT_MASK + (lowerBits - 1));
+		this.block.blastResistance = BASE_VALUE_BLAST / (float)Math.pow(4, (LOWER_BIT_MASK - lowerBits));
+		this.block.blockHardness = BASE_VALUE_HARDNESS / (float)Math.pow(4, (LOWER_BIT_MASK - lowerBits));
+		world.playBlockSoundEffect(null, x, y + 0.5F, z, Blocks.PERMAFROST, EnumBlockSoundEffectType.DIG);
+		world.scheduleBlockUpdate(x, y, z, this.id(), coolDown);
 	}
 
 
 	@Override
 	public void onBlockPlacedByWorld(World world, int x, int y, int z) {
 		int metadata = world.getBlockMetadata(x, y, z);
-		this.block.blastResistance = BASE_VALUE_BLAST / (float)Math.pow(4, (3 - (metadata & 0b0000_0011)));
-		this.block.blockHardness = BASE_VALUE_HARDNESS / (float)Math.pow(4, (3 - (metadata & 0b0000_0011)));
+		this.block.blastResistance = BASE_VALUE_BLAST / (float)Math.pow(4, (LOWER_BIT_MASK - (metadata & LOWER_BIT_MASK)));
+		this.block.blockHardness = BASE_VALUE_HARDNESS / (float)Math.pow(4, (LOWER_BIT_MASK - (metadata & LOWER_BIT_MASK)));
 		world.setBlockMetadata(x, y, z, this.getRandomMetadata(world));
 	}
 
--- src/main/java/jamdoggie/betterbattletowers/mixins/EntityMixinBlockStandOn.java
+++ src/main/java/jamdoggie/betterbattletowers/mixins/EntityMixinBlockStandOn.java
@@ -1,8 +1,55 @@
 package jamdoggie.betterbattletowers.mixins;
 
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import jamdoggie.betterbattletowers.block.BattleTowerTriggerStandOn;
+import net.minecraft.core.block.Block;
 import net.minecraft.core.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(value = Entity.class, remap = false)
-public class EntityMixinBlockStandOn{
+public abstract class EntityMixinBlockStandOn{
+
+	@Shadow
+	public int id;
+
+	@Definition(id = "muteStepSounds", field="Lnet/minecraft/core/entity/Entity;muteStepSounds:Z")
+	@Expression("?.muteStepSounds")
+	@ModifyExpressionValue(method = "move", at = @At("MIXINEXTRAS:EXPRESSION"))
+	public boolean insureAlwaysTriggerEvent(
+		boolean original,
+		@Local(name ="blockWalkedOn", ordinal = 0) Block<?> blockWalkedOn,
+		@Local(name = "blockX", ordinal = 0) int blockX,
+		@Local(name = "blockY", ordinal = 1) int blockY,
+		@Local(name = "blockZ", ordinal = 2) int blockZ
+	){
+		if(!original && blockWalkedOn != null && blockWalkedOn.getLogic() instanceof BattleTowerTriggerStandOn){
+			Entity asThis = (Entity) (Object) this;
+			blockWalkedOn.onEntityWalking(asThis.world, blockX, blockY, blockZ, asThis);
+		}
+		return original;
+	}
+
+	@Definition(id = "walkedSteps", local = @Local(type = int.class, ordinal = 4))
+	@Expression("walkedSteps > 0")
+	@ModifyExpressionValue(method = "move", at = @At("MIXINEXTRAS:EXPRESSION"))
+	public boolean onEntityStand(
+		boolean original,
+		@Local(name ="blockWalkedOn", ordinal = 0) Block<?> blockWalkedOn,
+		@Local(name = "blockX", ordinal = 0) int blockX,
+		@Local(name = "blockY", ordinal = 1) int blockY,
+		@Local(name = "blockZ", ordinal = 2) int blockZ
+	){
+		if(!original && blockWalkedOn != null && blockWalkedOn.getLogic() instanceof BattleTowerTriggerStandOn){
+			Entity asThis = (Entity) (Object) this;
+			BattleTowerTriggerStandOn triggeredBlock = (BattleTowerTriggerStandOn) blockWalkedOn.getLogic();
+			triggeredBlock.onEntityStandOn(asThis.world, blockX, blockY, blockZ, asThis);
+			return false;
+		}
+		return original;
+	}
 }
