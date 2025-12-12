fa6bc3bb0ec16ed21aaccf3813d1317f7ebca494
Redart15 <redart15@users.noreply.github.com>
2025-12-12 14:07:30 +0100

    adjusted the block hardness with damage level

--- src/main/java/jamdoggie/betterbattletowers/block/BlockLogicCrumbling.java
+++ src/main/java/jamdoggie/betterbattletowers/block/BlockLogicCrumbling.java
@@ -11,7 +11,7 @@ import net.minecraft.core.entity.player.Player;
 import net.minecraft.core.enums.EnumBlockSoundEffectType;
 import net.minecraft.core.enums.EnumDropCause;
 import net.minecraft.core.item.ItemStack;
-import net.minecraft.core.util.helper.Direction;
+import net.minecraft.core.util.helper.Side;
 import net.minecraft.core.world.World;
 import org.jetbrains.annotations.Nullable;
 
@@ -22,13 +22,11 @@ import static jamdoggie.betterbattletowers.worldgen.WorldFeatureTower.BLOCK_AIR;
 public class BlockLogicCrumbling extends BlockLogic implements BattleTowerTriggerStandOn {
 	public static final int UPPER_BIT_MASK = 0b1000_0000;
 	public static final int LOWER_BIT_MASK = 0b0000_0011;
-	private static final float BASE_VALUE_BLAST = 15.0f;
-	private static final float BASE_VALUE_HARDNESS = 2000.0f;
 	private final int coolDown;
 
 	public BlockLogicCrumbling(Block<?> block, Material material, float coolDown) {
 		super(block, material);
-		this.coolDown = (int)Math.floor(Math.max(coolDown * Global.TICKS_PER_SECOND, 1));
+		this.coolDown = (int) Math.floor(Math.max(coolDown * Global.TICKS_PER_SECOND, 1));
 	}
 
 	@Override
@@ -70,50 +68,32 @@ public class BlockLogicCrumbling extends BlockLogic implements BattleTowerTrigge
 			return;
 		}
 		world.setBlockMetadataWithNotify(x, y, z, UPPER_BIT_MASK + (lowerBits - 1));
-		this.block.blastResistance = BASE_VALUE_BLAST / (float)Math.pow(4, (LOWER_BIT_MASK - lowerBits));
-		this.block.blockHardness = BASE_VALUE_HARDNESS / (float)Math.pow(4, (LOWER_BIT_MASK - lowerBits));
 		world.playBlockSoundEffect(null, x, y + 0.5F, z, Blocks.PERMAFROST, EnumBlockSoundEffectType.DIG);
 		world.scheduleBlockUpdate(x, y, z, this.id(), coolDown);
 	}
 
-
 	@Override
-	public void onBlockPlacedByWorld(World world, int x, int y, int z) {
-		int metadata = world.getBlockMetadata(x, y, z);
-		this.block.blastResistance = BASE_VALUE_BLAST / (float)Math.pow(4, (LOWER_BIT_MASK - (metadata & LOWER_BIT_MASK)));
-		this.block.blockHardness = BASE_VALUE_HARDNESS / (float)Math.pow(4, (LOWER_BIT_MASK - (metadata & LOWER_BIT_MASK)));
-		world.setBlockMetadata(x, y, z, this.getRandomMetadata(world));
-	}
-
-	private int getRandomMetadata(World world){
-		int rand = world.rand.nextInt(10);
-		if(rand >= 5) return 2;
-		if(rand >= 2) return 1;
-		return 0;
+	public float blockStrength(World world, int x, int y, int z, Side side, Player player) {
+		int metadata = world.getBlockMetadata(x, y, z) & LOWER_BIT_MASK;
+		float blockHardness = this.block.blockHardness / Math.max(2 - (metadata & LOWER_BIT_MASK), 1);
+		if (!player.canHarvestBlock(this.block)) {
+			return 1.0F / blockHardness / 100.0F;
+		}
+		return player.getCurrentPlayerStrVsBlock(this.block) / blockHardness / 30.0F;
 	}
 
 	private void spawnParticle(World world, int x, int y, int z) {
 		Random random = world.rand;
 		for (int i = 0; i < 16; ++i) {
-			Direction face = Direction.values()[random.nextInt(6)];
+			double fx = x + (random.nextDouble() * 0.6 - 0.3);
+			double fy = y + 0.5;
+			double fz = z + (random.nextDouble() * 0.6 - 0.3);
 
-			double faceX = x + (random.nextDouble() * 0.6 - 0.3);
-			double faceY = y + 0.5 + (random.nextDouble() * 0.6 - 0.3);
-			double faceZ = z + (random.nextDouble() * 0.6 - 0.3);
+			double dx = fx * 0.3 + (random.nextDouble() - 0.5) * 0.2;
+			double dy = fy * 0.3 + (random.nextDouble() - 0.5) * 0.2;
+			double dz = fz * 0.3 + (random.nextDouble() - 0.5) * 0.2;
 
-			double offX = face.getOffsetX() * (random.nextDouble() * 0.3);
-			double offY = face.getOffsetY() * (random.nextDouble() * 0.3);
-			double offZ = face.getOffsetZ() * (random.nextDouble() * 0.3);
-
-			double spawnX = faceX + offX;
-			double spawnY = faceY + offY;
-			double spawnZ = faceZ + offZ;
-
-			double vx = offX * 0.3 + (random.nextDouble() - 0.5) * 0.2;
-			double vy = offY * 0.3 + (random.nextDouble() - 0.5) * 0.2;
-			double vz = offZ * 0.3 + (random.nextDouble() - 0.5) * 0.2;
-
-			world.spawnParticle("block", spawnX, spawnY, spawnZ, vx, vy, vz, this.id());
+			world.spawnParticle("block", fx, fy, fz, dx, dy, dz, this.id());
 		}
 	}
 
