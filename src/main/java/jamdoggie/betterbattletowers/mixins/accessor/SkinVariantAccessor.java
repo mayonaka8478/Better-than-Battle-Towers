b2791f9c505e0452653f8d005c446f73bb30f676
Redart15 <redart15@users.noreply.github.com>
2025-12-30 20:51:43 +0100

    removed unused mixin and added accessors

---
+++ src/main/java/jamdoggie/betterbattletowers/mixins/accessor/SkinVariantAccessor.java
@@ -0,0 +1,12 @@
+package jamdoggie.betterbattletowers.mixins.accessor;
+
+import net.minecraft.client.entity.ClientSkinVariantList;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(value = ClientSkinVariantList.class, remap = false)
+public interface SkinVariantAccessor {
+
+	@Invoker(value = "getEntityVariants")
+	ClientSkinVariantList.EntityVariants invokeGetEntityVariants(String jsonPath);
+}
