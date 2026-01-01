4d3e396e6d1d9b2f9d73905a8eec079173574c6b
Redart15 <redart15@users.noreply.github.com>
2026-01-01 20:37:57 +0100

    made the table use hocon

--- src/main/java/jamdoggie/betterbattletowers/config/LootTables.java
+++ src/main/java/jamdoggie/betterbattletowers/config/LootTables.java
@@ -3,13 +3,17 @@ package jamdoggie.betterbattletowers.config;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import jamdoggie.betterbattletowers.config.util.DefaultTable;
-import jamdoggie.betterbattletowers.config.util.LootEntry;
-import jamdoggie.betterbattletowers.config.util.LootEntryJsonAdapter;
+import com.google.gson.JsonSyntaxException;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigException;
+import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigRenderOptions;
+import jamdoggie.betterbattletowers.config.util.*;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.core.WeightedRandomBag;
 import net.minecraft.core.WeightedRandomLootObject;
 import net.minecraft.core.block.BlockLogicChest;
+import net.minecraft.core.data.DataLoader;
 import net.minecraft.core.data.registry.recipe.adapter.ItemStackJsonAdapter;
 import net.minecraft.core.data.registry.recipe.adapter.WeightedRandomLootObjectJsonAdapter;
 import net.minecraft.core.item.*;
@@ -20,60 +24,93 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
 
+import static jamdoggie.betterbattletowers.BattleTowerMod.LOGGER;
 import static jamdoggie.betterbattletowers.BattleTowerMod.MOD_ID;
 
-public class LootTable {
+public class LootTables {
 	public static final int LAPIZ = DyeColor.BLUE.itemMeta;
 	public static final double INC = 1.20f; // 1.29f
 	public static final int MAX_TIER = 9;
+	public static final Path CONFIG_PATH = FabricLoader.getInstance().getConfigDir().resolve(MOD_ID + "loot.conf");
 	@SuppressWarnings("java:S1905")
 	protected static final WeightedRandomBag<WeightedRandomLootObject>[] TOWER_LOOT_TABLE = (WeightedRandomBag<WeightedRandomLootObject>[]) new WeightedRandomBag[10];
+	public static final String ASSETS_JAR_PATH = String.format("/assets/%s/loot_table/%sloot.conf", MOD_ID, MOD_ID);
 	private static boolean init = false;
 
-	private LootTable(){}
+	private LootTables() {
+	}
 
 	public static void init() {
 		if (init) {
 			return;
 		}
 		init = true;
-		Path path = FabricLoader.getInstance().getConfigDir().resolve(MOD_ID + "_loot.json");
-		Gson gson = new GsonBuilder().registerTypeAdapter(LootEntry.class, new LootEntryJsonAdapter())
+		Gson gson = new GsonBuilder()
+			.registerTypeAdapter(LootTable.class, new LootTableJsonAdapter())
+			.registerTypeAdapter(LootEntry.class, new LootEntryJsonAdapter())
 			.registerTypeAdapter(WeightedRandomLootObject.class, new WeightedRandomLootObjectJsonAdapter())
 			.registerTypeAdapter(ItemStack.class, new ItemStackJsonAdapter())
 			.setPrettyPrinting()
 			.create();
-		Type type = new TypeToken<List<List<LootEntry>>>() {}.getType();
+		Type type = new TypeToken<List<LootTable>>() {
+		}.getType();
 
-		if(Files.notExists(path)){
-			try{
-				List<List<LootEntry>> defaultTable = DefaultTable.getDefaultTable();
-				String json = gson.toJson(defaultTable, type);
-				Files.write(path, json.getBytes(StandardCharsets.UTF_8));
-			}catch (IOException e){
+		if (Files.notExists(CONFIG_PATH)) {
+			try {
+				if(FabricLoader.getInstance().isDevelopmentEnvironment() && DataLoader.class.getResourceAsStream(ASSETS_JAR_PATH) == null){
+					List<LootTable> defaultTable = DefaultTable.getDefaultTable();
+					String json = gson.toJson(defaultTable, type);
+					Config config = ConfigFactory.parseString("{ loot_tables = " + json + " }");
+					Files.write(CONFIG_PATH, config.root()
+						.render(ConfigRenderOptions.defaults()
+							.setJson(false)
+							.setOriginComments(false))
+						.getBytes(StandardCharsets.UTF_8));
+				}
+				try (InputStream stream = DataLoader.class.getResourceAsStream(ASSETS_JAR_PATH)) {
+					if (stream == null) {
+						LOGGER.error("LootTable could not be regenerated, betterbattletowers.jar is corrupted.");
+						throw new RuntimeException("Asset not found in JAR: " + ASSETS_JAR_PATH);
+					}
+					Files.copy(stream, CONFIG_PATH);
+				}
+			} catch (IOException e) {
+				LOGGER.error("Loot table failed to be copied to config.");
 				throw new RuntimeException("Failed to create default loot config at: "
-					+ path.toAbsolutePath(), e);
+					+ CONFIG_PATH.toAbsolutePath(), e);
 			}
 		}
 
 		try {
-			String jsonString;
-			byte[] bytes = Files.readAllBytes(path);
-			jsonString = new String(bytes, StandardCharsets.UTF_8);
-			createTables(gson.fromJson(jsonString, type));
-		} catch (IOException e) {
-			throw new RuntimeException("Failed to read loot configuration file at:" + path.toAbsolutePath(), e);
+			Config config = ConfigFactory.parseFile(CONFIG_PATH.toFile());
+			String jsonString = config.getValue("loot_tables")
+				.render(ConfigRenderOptions.concise());
+			List<LootTable> table = gson.fromJson(jsonString, type);
+			createTables(table);
+		} catch (ConfigException e) {
+			LOGGER.error("Failed to read configuration file.");
+			throw new RuntimeException("Failed to parse HOCON config: " + CONFIG_PATH.toAbsolutePath(), e);
+		} catch (JsonSyntaxException e) {
+			LOGGER.error("");
+			throw new RuntimeException("Loot configuration file is malformed: " + CONFIG_PATH.toAbsolutePath(), e);
 		}
 	}
 
-	private static void createTables(List<List<LootEntry>> lootEntries) {
+	private static void createTables(List<LootTable> lootEntries) {
+		Map<Integer, List<LootEntry>> table = new HashMap<>();
+		for (LootTable subTable : lootEntries) {
+			table.put(subTable.getLevel(), subTable.getLootEntries());
+		}
+
 		double level = 1;
+		List<LootEntry> lastNonEmpty = null;
 		for (int jndex = 0; jndex < 10; jndex++, level *= INC) {
 			TOWER_LOOT_TABLE[jndex] = new WeightedRandomBag<>();
 			if (jndex > 0) {
@@ -81,8 +118,14 @@ public class LootTable {
 					TOWER_LOOT_TABLE[jndex].addEntry(entry.getObject(), entry.getWeight());
 				}
 			}
-			List<LootEntry> lootEntryList = lootEntries.get(jndex);
-			if (lootEntryList.isEmpty()) continue;
+			List<LootEntry> lootEntryList = table.get(jndex);
+			if (lootEntryList.isEmpty()) {
+				if(lastNonEmpty == null){
+					continue;
+				}
+				lootEntryList = lastNonEmpty;
+			}
+			lastNonEmpty = lootEntryList;
 			for (LootEntry entry : lootEntryList) {
 				TOWER_LOOT_TABLE[jndex].addEntry(entry.getLootObj(), level * entry.getWeight());
 			}
@@ -94,7 +137,7 @@ public class LootTable {
 		if (inventory == null) return;
 		List<ItemStack> stacks = generateLootList(random, tier, lootAmount);
 		for (ItemStack stack : stacks) {
-			LootTable.placeItemInChest(random, stack, inventory);
+			LootTables.placeItemInChest(random, stack, inventory);
 		}
 	}
 
---
+++ src/main/java/jamdoggie/betterbattletowers/config/util/LootTableJsonAdapter.java
@@ -0,0 +1,32 @@
+package jamdoggie.betterbattletowers.config.util;
+
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LootTableJsonAdapter implements JsonDeserializer<LootTable>, JsonSerializer<LootTable> {
+	@Override
+	public LootTable deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+		JsonObject obj = json.getAsJsonObject();
+		int level = obj.get("floor").getAsInt();
+		List<LootEntry> lootEntries = new ArrayList<>();
+		JsonArray entriesArray = obj.getAsJsonArray("table");
+		for (JsonElement element : entriesArray) {
+			LootEntry entry = context.deserialize(element, LootEntry.class);
+			lootEntries.add(entry);
+		}
+		return new LootTable(level, lootEntries);
+	}
+
+	@Override
+	public JsonElement serialize(LootTable src, Type typeOfSrc, JsonSerializationContext context) {
+		JsonObject obj = new JsonObject();
+		obj.addProperty("floor", src.getLevel());
+		JsonArray entriesArray = new JsonArray();
+		src.getLootEntries().stream().map(context::serialize).forEach(entriesArray::add);
+		obj.add("table", entriesArray);
+		return obj;
+	}
+}
