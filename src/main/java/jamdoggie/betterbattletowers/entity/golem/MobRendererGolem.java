c1daef6a77694e020cd92f7097442485eb6c625d
Redart15 <redart15@users.noreply.github.com>
2026-01-06 21:13:41 +0100

    overgrown golem foliage changes color based on leaves colorizer

--- src/main/java/jamdoggie/betterbattletowers/entity/golem/MobRendererGolem.java
+++ src/main/java/jamdoggie/betterbattletowers/entity/golem/MobRendererGolem.java
@@ -1,9 +1,11 @@
 package jamdoggie.betterbattletowers.entity.golem;
 
 import net.minecraft.client.render.LightmapHelper;
+import net.minecraft.client.render.block.color.BlockColorDispatcher;
 import net.minecraft.client.render.entity.MobRenderer;
 import net.minecraft.client.render.model.ModelBiped;
 import net.minecraft.client.render.tessellator.Tessellator;
+import net.minecraft.core.block.Blocks;
 import org.lwjgl.opengl.GL11;
 
 import static jamdoggie.betterbattletowers.BattleTowerMod.MOD_ID;
@@ -25,15 +27,35 @@ public class MobRendererGolem extends MobRenderer<MobGolem> {
 	protected boolean prepareArmor(MobGolem golem, int renderPass, float partialTick) {
 		if (renderPass == 0 && !golem.isDormant()) {
 			this.bindTexture(String.format("/assets/%s/textures/entity/golem/%s/eyes/%s.png", MOD_ID, golem.getEntityData().getString(3), golem.getTextureReference()));
-			if (LightmapHelper.isLightmapEnabled()) {
-				LightmapHelper.setLightmapCoord(LightmapHelper.getLightmapCoord(15, 15));
-			}
+			enableFullBright();
 			GL11.glDisable(GL11.GL_BLEND);
 			GL11.glEnable(GL11.GL_ALPHA_TEST);
 			GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 			return true;
 		}
+		if(renderPass == 1 && golem.getEntityData().getString(3).equalsIgnoreCase("overgrown")){
+			disableFullBrigth(golem, partialTick);
+			this.bindTexture(String.format("/assets/%s/textures/entity/golem/%s/leaves.png", MOD_ID, golem.getEntityData().getString(3)));
+			int color = BlockColorDispatcher.getInstance().getDispatch(Blocks.GRASS).getWorldColor(golem.world, (int)Math.floor(golem.x), (int)Math.floor(golem.y), (int)Math.floor(golem.z));
+			float r = (color >> 16 & 255) / 255.0F;
+			float g = (color >> 8 & 255) / 255.0F;
+			float b = (color & 255) / 255.0F;
+			GL11.glColor4f(r, g, b, 1.0F);
+			return true;
+		}
 		return false;
+	}
+
+	private static void enableFullBright() {
+		if (LightmapHelper.isLightmapEnabled()) {
+			LightmapHelper.setLightmapCoord(LightmapHelper.getLightmapCoord(15, 15));
+		}
+	}
+
+	private static void disableFullBrigth(MobGolem golem, float partialTick) {
+		if (LightmapHelper.isLightmapEnabled()) {
+			LightmapHelper.setLightmapCoord(golem.getLightmapCoord(partialTick));
+		}
 	}
 
 	@Override
