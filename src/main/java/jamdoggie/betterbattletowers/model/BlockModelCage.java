cc50d7ed0b8b8d9533ac26c43e734421c60c8f04
Redart15 <redart15@users.noreply.github.com>
2026-01-29 22:00:01 +0100

    adjusted model for cage

--- src/main/java/jamdoggie/betterbattletowers/model/BlockModelCage.java
+++ src/main/java/jamdoggie/betterbattletowers/model/BlockModelCage.java
@@ -1,8 +1,11 @@
 package jamdoggie.betterbattletowers.model;
 
 import net.minecraft.client.render.block.model.BlockModelStandard;
+import net.minecraft.client.render.tessellator.Tessellator;
 import net.minecraft.core.block.Block;
 import net.minecraft.core.block.BlockLogic;
+import net.minecraft.core.util.phys.AABB;
+import net.minecraft.core.world.WorldSource;
 
 public class BlockModelCage<T extends BlockLogic> extends BlockModelStandard<T> {
 	private final boolean renderInside;
@@ -11,4 +14,20 @@ public class BlockModelCage<T extends BlockLogic> extends BlockModelStandard<T> 
 		super(block);
 		this.renderInside = renderInside;
 	}
+
+	@Override
+	public boolean render(Tessellator tessellator, int x, int y, int z) {
+		AABB bounds = this.block.getBounds();
+		double offset = 0.0001;
+		double oneOffset = 1 - offset;
+		bounds.set(offset, offset, offset, oneOffset, oneOffset, oneOffset);
+		this.renderStandardBlock(tessellator, bounds, x, y, z);
+		return true;
+	}
+
+	@Override
+	public boolean shouldSideBeRendered(WorldSource blockAccess, AABB bounds, int x, int y, int z, int side) {
+		return this.renderInside || blockAccess.getBlockId(x, y, z) != this.block.id();
+	}
+
 }
